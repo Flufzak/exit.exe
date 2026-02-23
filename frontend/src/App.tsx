c2313fc7e56d3cd2588e87a83524bc4f9a0f14c2
Flufzak <khachatryan.suzi@gmail.com>
2026-02-23 08:51:50 +0100

    added custom styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
-import './styles/App.css'
+import Loader from "./components/ui/Loader";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="app">
       <div className="container">
         <h1>Exit.exe</h1>
+        <Loader />
         <p className="message">UI scaffold ready</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
